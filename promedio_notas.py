218299e642022f0bd084322f9f81a8596217cd24
Pamela <guambanataly38@gmail.com>
2026-09-07 12:16:48 -0500

    Add files via upload

---
+++ promedio_notas.py
@@ -0,0 +1,12 @@
+def calcular_promedio(nota1, nota2):
+    promedio = (nota1 + nota2) / 2
+    return promedio
+
+
+if __name__ == "__main__":
+    nota1 = 8
+    nota2 = 10
+
+    resultado = calcular_promedio(nota1, nota2)
+
+    print("El promedio es:", resultado)
